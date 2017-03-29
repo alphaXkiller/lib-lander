910afe56ef2d60949676bd1e1e70b8a6f1ae80ab
Franklin Huang <frankie0628@gmail.com>
2017-03-29 15:28:09 -0700

    more style on drawer menu

--- src/components/shared-components/grid.js
+++ src/components/shared-components/grid.js
@@ -1,8 +1,11 @@
 import R from 'ramda'
 import React from 'react'
 
-const Grid = () => (
-  <div className='large-up-7 gridSet'>
+const Grid = ({zIndex} = {}) => (
+  <div 
+    className='large-up-7 gridSet' 
+    style={ R.isNil(zIndex )? {} : {zIndex}}
+  >
     {
       R.times( id => (
         <div key={id} className='columns'><div className='hrv'/></div>
