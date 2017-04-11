e6f6dca0c1f3fb015ecb5667ed7c6856272a6c98
Franklin Huang <frankie0628@gmail.com>
2017-04-11 13:38:13 -0700

    404 page not found

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,16 @@ import R                   from 'ramda'
 import React               from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
-import Home from './containers/public/home.js'
-import Lineup from './containers/public/lineup.js'
-import Vibe from './containers/public/vibe.js'
+import Home     from './containers/public/home.js'
+import Lineup   from './containers/public/lineup.js'
+import Vibe     from './containers/public/vibe.js'
+import NotFound from './containers/public/404.js'
 
 const RouteFunctor = [
   { path: '/', component: Home, exact: true},
   { path: '/lineup', component: Lineup },
   { path: '/vibe', component: Vibe },
+  { path: '*', component: NotFound }
 ]
 
 
