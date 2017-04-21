a873023e989b24e49a8a6926e000b6613528fb48
Lucho Suarez <luis.suarez@rrpartners.com>
2017-04-21 15:55:53 -0700

    Quick fix for the Safari issue with the colors.

---
+++ src/constants/cssColors.js
@@ -0,0 +1,15 @@
+const colors = [
+  'pink_on',
+  'pink_off',
+  'blue_on',
+  'blue_off',
+  'yellow_on',
+  'yellow_off',
+  'green_on',
+  'green_off',
+  'green_off'
+]
+
+export default {
+  colors
+}
