08060b83a988fd2957c08c2f1fd426f716e70b64
Franklin Huang <frankie0628@gmail.com>
2017-04-25 16:41:35 -0700

    redirect link

--- src/actions/page.js
+++ src/actions/page.js
@@ -1,11 +1,14 @@
+import R          from 'ramda'
 import ApiRequest from '../lib/restful'
 
 const REQUEST_PAGE = 'REQUEST_PAGE'
 const RECEIVE_PAGE = 'RECEIVE_PAGE'
+const PAGE_NOT_FOUND = 'PAGE_NOT_FOUND'
 const PAGE        = 'page'
 
 const requestPage = () => ({type: REQUEST_PAGE})
 const receivePage = payload => ({type: RECEIVE_PAGE, payload})
+const pageNotFound = () => ({type: PAGE_NOT_FOUND})
 
 const fetchPage = ({keys}) => (dispatch, getState) => {
   dispatch(requestPage())
@@ -14,15 +17,22 @@ const fetchPage = ({keys}) => (dispatch, getState) => {
     method: 'get',
     path: PAGE,
     keys
-  }).then( response => {
-    dispatch(receivePage(response.data))
   })
+    .then( response => {
+      dispatch(receivePage(response.data))
+    })
+
+    .catch( err => {
+      if (err.response.status === 404)
+        dispatch(pageNotFound())
+    })
 
 }
 
 export default {
   REQUEST_PAGE,
   RECEIVE_PAGE,
+  PAGE_NOT_FOUND,
   PAGE,
   requestPage,
   receivePage,
--- src/containers/public/404.js
+++ src/containers/public/404.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import Loader from '../../img/lib_loader.png'
 
 class NotFound extends React.Component {
   render() {
     return(
-      <div className='content'>
-        <h1>404 page not found</h1>
-        <h3>We are sorry but the page you are looking for does not exist.</h3>
+      <div className='content content-page'>
+        <div className='row align-center'>
+          <div className='small-14 large-10 large-push-2 column'>
+            <div className='row page-title'>
+              <div className='large-7 column'>
+                <h1>404 page not found</h1>
+                <h3>We are sorry but the page you are looking for does not exist.</h3>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='large-10 large-push-2 column small-14'>
+                <img src={`/assets/${Loader}`}/>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
