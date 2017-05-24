29b49037695cdf87ba8d0e0ee73af2d3f7942593
Franklin Huang <frankie0628@gmail.com>
2017-05-24 14:59:50 -0700

    lineup action

--- src/lib/restful-map.js
+++ src/lib/restful-map.js
@@ -1,13 +1,14 @@
 export default {
 
-  'vibe'     : '/lib/vibe',
-  'landing'  : '/lib/landing',
-  'planning' : '/lib/plan',
-  'news'     : '/lib/news',
-  'post'     : '/lib/news/:slug',
-  'ticket'   : '/lib/ticket',
-  'page'     : '/lib/page/:slug',
-  'faq'      : '/lib/faq',
-  'lineup'   : '/lib/lineup',
-  'sponsor'  : '/lib/sponsor'
+  'vibe'           : '/lib/vibe',
+  'landing'        : '/lib/landing',
+  'planning'       : '/lib/plan',
+  'news'           : '/lib/news',
+  'post'           : '/lib/news/:slug',
+  'ticket'         : '/lib/ticket',
+  'page'           : '/lib/page/:slug',
+  'faq'            : '/lib/faq',
+  'lineup'         : '/lib/lineup',
+  'lineup_by_slug' : '/lib/lineup/:slug',
+  'sponsor'        : '/lib/sponsor'
 }
