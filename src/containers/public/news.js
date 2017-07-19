dd6201e4dc435d2def813e8a5a0335580c3189fb
Franklin Huang <frankie0628@gmail.com>
2017-07-19 09:23:34 -0700

    add a trigger to run the embed script

--- src/containers/public/news.js
+++ src/containers/public/news.js
@@ -18,9 +18,9 @@ const _renderNewRow = news_list => {
           <h2 className='planning-title left-title'>
             {
               news_left.is_external_link ?
-                <a href={news_left.external_link} target='_blank'>{news_left.title}</a>
+                <a href={news_left.external_link}>{news_left.title}</a>
               :
-                <Link to={`/news/${news_left.slug}`}>{news_left.title}</Link>
+                <a href={`/news/${news_left.slug}`}>{news_left.title}</a>
             }
           </h2>
           <div className='planning-img'><img src={news_left.image} /></div>
@@ -31,9 +31,9 @@ const _renderNewRow = news_list => {
             <button className='btn-underline default'>
               {
                 news_left.is_external_link ?
-                  <a href={news_left.external_link} target='_blank'>{news_left.link_text}</a>
+                  <a href={news_left.external_link}>{news_left.link_text}</a>
                 :
-                  <Link to={`/news/${news_left.slug}`}>{news_left.link_text}</Link>
+                  <a href={`/news/${news_left.slug}`}>{news_left.link_text}</a>
               }
               <hr/>
             </button>
@@ -47,9 +47,9 @@ const _renderNewRow = news_list => {
               <h2 className='planning-title'>
               {
                 news_right.is_external_link ?
-                  <a href={news_right.external_link} target='_blank'>{news_right.title}</a>
+                  <a href={news_right.external_link}>{news_right.title}</a>
                 :
-                  <Link to={`/news/${news_right.slug}`}>{news_right.title}</Link>
+                  <a href={`/news/${news_right.slug}`}>{news_right.title}</a>
               }
               </h2>
               <div className='planning-img'><img src={news_right.image} /></div>
@@ -60,9 +60,9 @@ const _renderNewRow = news_list => {
                 <button className='btn-underline default'>
                   {
                     news_right.is_external_link ?
-                      <a href={news_right.external_link} target='_blank'>{news_right.link_text}</a>
+                      <a href={news_right.external_link}>{news_right.link_text}</a>
                     :
-                      <Link to={`/news/${news_right.slug}`}>{news_right.link_text}</Link>
+                      <a href={`/news/${news_right.slug}`}>{news_right.link_text}</a>
                   }
                   <hr className='pink'/>
                 </button>
--- src/containers/public/newsPost.js
+++ src/containers/public/newsPost.js
@@ -5,8 +5,38 @@ import { connect }  from 'react-redux'
 import { Post } from '../../actions/index'
 import { createMarkup, loadingLogo } from '../../lib/helpers'
 
+const triggerInstEmbed = () => {
+  const script = document.createElement('script')
+  script.src = 'https://platform.instagram.com/en_US/embeds.js'
+  script.async = true
+  script.id = 'embedScript'
+
+  const blockquote = document.createElement('blockquote')
+  blockquote.className = 'instagram-media'
+
+  const link = document.createElement('a')
+  link.href = 'https://www.instagram.com/p/BS7JvgDlErQ/'
+  blockquote.appendChild(link)
+
+  document.body.appendChild(script)
+  document.body.appendChild(blockquote)
+}
+
+const hasInstBlockquote = R.test(/<blockquote class="instagram-media"/g)
+
+const removeTrigger = () => {
+  // remove trigger iframe
+  document.getElementById('body').lastChild.remove()
+}
 
 class newsPost extends Component {
+  constructor(){
+    super()
+    this.state = {
+      runScript : false
+    }
+  }
+
   componentDidMount() {
     const keys = {slug: this.props.match.params.slug}
     this.props.fetchPost({keys})
@@ -16,6 +46,16 @@ class newsPost extends Component {
     if (prevProps.match.params.slug !== this.props.match.params.slug) {
       const keys = {slug: this.props.match.params.slug}
       this.props.fetchPost({keys})
+    }
+
+    if (this.props.post.description) {
+      // If description has instagram embedded, run the trigger
+      if (hasInstBlockquote(this.props.post.description) && !this.state.runScript)
+        this.setState({runScript: true}, () => triggerInstEmbed())
+      // If the trigger already ran, remove the trigger iframe
+      if (this.state.runScript && document.getElementById('body').lastChild.tagName === 'IFRAME') {
+        removeTrigger()
+      }
     }
   }
 
